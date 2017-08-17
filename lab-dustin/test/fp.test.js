485bc68a0fadd0d074431eb3ecca46e5dd0ec309
Dustin <dyelland@gmail.com>
2017-08-17 15:47:46 -0500

    Wrote a test for reduce function

--- lab-dustin/test/fp.test.js
+++ lab-dustin/test/fp.test.js
@@ -3,10 +3,22 @@ const { expect } = require('chai');
 const sut = require('../lib/fp.js');
 
 describe('Module', function(){
-  describe('#mapFunction', function(){
+  describe('#mapFunction()', function(){
     it('should return new array', function(done){
       sut.mapFunction([1,2,3], function(array){
         expect(array).to.be.an('array');
+        var expectedArray = [2,4,6];
+        array.forEach(function(item,i){
+          assert.equal(array[i], expectedArray[i]);
+        });
+        done();
+      });
+    });
+  });
+  describe('#reduceFunction()',function(){
+    it('shoud return a single numerical value', function(done){
+      sut.reduceFunction([1,2,3], function(reduceVal){
+        expect(reduceVal).to.be.a('number');
         done();
       });
     });
